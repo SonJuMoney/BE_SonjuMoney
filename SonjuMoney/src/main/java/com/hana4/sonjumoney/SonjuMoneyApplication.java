8dfcf018f5847bbb480b61707126886293255658
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2025-01-06 14:57:17 +0900

    spring boot setting

---
+++ SonjuMoney/src/main/java/com/hana4/sonjumoney/SonjuMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.hana4.sonjumoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonjuMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonjuMoneyApplication.class, args);
+	}
+
+}
