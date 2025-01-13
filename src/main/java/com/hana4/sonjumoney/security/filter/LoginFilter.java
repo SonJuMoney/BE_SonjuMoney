2b330a1dbd7f9194e30069bb75ac3e614de3c11c
hs917ouo <hs917ouo@naver.com>
2025-01-13 18:23:45 +0900

    [Feat] CustomUserDetails 구현

--- src/main/java/com/hana4/sonjumoney/security/filter/LoginFilter.java
+++ src/main/java/com/hana4/sonjumoney/security/filter/LoginFilter.java
@@ -1,4 +1,28 @@
-package com.hana4.sonjumoney.security.filter;
-
-public class LoginFilter {
-}
+// package com.hana4.sonjumoney.security.filter;
+//
+// import java.io.IOException;
+//
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+// import com.hana4.sonjumoney.security.util.JwtUtil;
+//
+// import jakarta.servlet.FilterChain;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import lombok.RequiredArgsConstructor;
+//
+// @RequiredArgsConstructor
+// public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+//
+// 	private final AuthenticationManager authenticationManager;
+// 	private final JwtUtil jwtUtil;
+//
+// 	@Override
+// 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+// 		Authentication authResult) throws IOException, ServletException {
+// 		String authId = authResult.getDetails().toString();
+// 	}
+// }
