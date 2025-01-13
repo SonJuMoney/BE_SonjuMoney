86dcb9ae41a868ef2853d3af7c9f0fc0ba7904ed
hs917ouo <hs917ouo@naver.com>
2025-01-13 10:36:00 +0900

    feat : family entity 추가

--- src/main/java/com/hana4/sonjumoney/domain/Family.java
+++ src/main/java/com/hana4/sonjumoney/domain/Family.java
@@ -1,4 +1,26 @@
 package com.hana4.sonjumoney.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "Family")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Family {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "family_id", nullable = false)
+	private Long id;
+
+	@Column(name = "family_name", nullable = false, columnDefinition = "VARCHAR(20)")
+	private Long familyName;
 }
