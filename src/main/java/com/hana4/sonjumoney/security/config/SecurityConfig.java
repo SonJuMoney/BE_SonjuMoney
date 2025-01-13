6bc85afe51bc51f687f7eb6062c110419aabd6a8
hs917ouo <hs917ouo@naver.com>
2025-01-13 21:09:22 +0900

    [Feat] LoginFilter 구현

--- src/main/java/com/hana4/sonjumoney/security/config/SecurityConfig.java
+++ src/main/java/com/hana4/sonjumoney/security/config/SecurityConfig.java
@@ -2,14 +2,26 @@ package com.hana4.sonjumoney.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.hana4.sonjumoney.security.filter.LoginFilter;
+import com.hana4.sonjumoney.security.util.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtUtil jwtUtil;
+	private final AuthenticationConfiguration authenticationConfiguration;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -19,6 +31,22 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+		http.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/api/auth/sign-up", "/api/auth/sign-in", "/api/auth/id-duplication",
+					"/api/auth/reissue")
+				.permitAll()
+				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui/index.html").permitAll()
+				.anyRequest()
+				.authenticated()
+			)
+			.addFilter(new LoginFilter(authenticationManager(), jwtUtil));
+
 		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
--- src/main/java/com/hana4/sonjumoney/security/filter/LoginFilter.java
+++ src/main/java/com/hana4/sonjumoney/security/filter/LoginFilter.java
@@ -1,28 +1,76 @@
-// package com.hana4.sonjumoney.security.filter;
-//
-// import java.io.IOException;
-//
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.core.Authentication;
-// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-// import com.hana4.sonjumoney.security.util.JwtUtil;
-//
-// import jakarta.servlet.FilterChain;
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import lombok.RequiredArgsConstructor;
-//
-// @RequiredArgsConstructor
-// public class LoginFilter extends UsernamePasswordAuthenticationFilter {
-//
-// 	private final AuthenticationManager authenticationManager;
-// 	private final JwtUtil jwtUtil;
-//
-// 	@Override
-// 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-// 		Authentication authResult) throws IOException, ServletException {
-// 		String authId = authResult.getDetails().toString();
-// 	}
-// }
+package com.hana4.sonjumoney.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hana4.sonjumoney.dto.request.SignInRequest;
+import com.hana4.sonjumoney.security.model.CustomUserDetails;
+import com.hana4.sonjumoney.security.util.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+	private final AuthenticationManager authenticationManager;
+	private final JwtUtil jwtUtil;
+
+	public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+		setFilterProcessesUrl("/api/auth/sign-in");
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
+		AuthenticationException {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			SignInRequest signInRequest = objectMapper.readValue(request.getInputStream(), SignInRequest.class);
+
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				signInRequest.authId(),
+				signInRequest.password()
+			);
+
+			return authenticationManager.authenticate(authenticationToken);
+		} catch (Exception e) {
+			throw new AuthenticationServiceException(e.getMessage());
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+		Authentication authResult) throws IOException, ServletException {
+
+		CustomUserDetails userDetails = (CustomUserDetails)authResult.getPrincipal();
+
+		String authId = userDetails.getAuthId();
+		Long userId = userDetails.getUserId();
+
+		String accessToken = jwtUtil.generateAccessToken(authId, userId);
+		String refreshToken = jwtUtil.generateRefreshToken(authId, userId);
+
+		response.addHeader("Authorization", "Bearer " + accessToken);
+		response.setHeader("Refresh-Token", refreshToken);
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().write("login successful");
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException failed) throws IOException, ServletException {
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.getWriter().write("login unsuccessful");
+	}
+}
