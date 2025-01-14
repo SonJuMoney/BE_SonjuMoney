7656c6c99c64392ca6fa4179bab03ff41fdd3fb9
ImHyungsuk <limhs8630@gmail.com>
2025-01-14 15:39:29 +0900

    [Feat] User의 자유입출금 계좌 조회 추가

--- src/main/java/com/hana4/sonjumoney/repository/AccountRepository.java
+++ src/main/java/com/hana4/sonjumoney/repository/AccountRepository.java
@@ -1,10 +1,15 @@
 package com.hana4.sonjumoney.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hana4.sonjumoney.domain.Account;
+import com.hana4.sonjumoney.domain.enums.AccountType;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Optional<Account> findByUser_IdAndAccountType_AccountType(Long userId, AccountType accountType);
 }
