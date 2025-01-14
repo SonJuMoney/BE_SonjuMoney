929b9b11feb782210cd9602c324e09ec0eefabc5
ImHyungsuk <limhs8630@gmail.com>
2025-01-14 15:38:46 +0900

    [Feat] 잔액부족 exception 추가

--- src/main/java/com/hana4/sonjumoney/exception/ErrorCode.java
+++ src/main/java/com/hana4/sonjumoney/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
 
 	//400
 	BAD_REQUEST(40000, HttpStatus.BAD_REQUEST, "잘못된 요청 형식입니다."),
+	INSUFFICIENT_BALANCE(40001, HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
 
 	//401
 	UNAUTHORIZED(40100, HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
@@ -29,7 +30,9 @@ public enum ErrorCode {
 
 	// 500
 	INTERNAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버내부 오류입니다."),
-	IMAGE_UPLOAD_ERROR(50001, HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+	IMAGE_UPLOAD_ERROR(50001, HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다."),
+	TRANSACTION_FAILED(50002, HttpStatus.INTERNAL_SERVER_ERROR, "거래가 실패했습니다."),
+	;
 	private final Integer code;
 	private final HttpStatus httpStatus;
 	private final String message;
