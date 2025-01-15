b001e8c05351fdfc9213076d9d39f6409c6723a6
MIJIN <larchlarix0309@gmail.com>
2025-01-15 12:56:14 +0900

    [Fix] EventAddRequest 수정

--- src/main/java/com/hana4/sonjumoney/dto/request/EventAddRequest.java
+++ src/main/java/com/hana4/sonjumoney/dto/request/EventAddRequest.java
@@ -3,6 +3,7 @@ package com.hana4.sonjumoney.dto.request;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hana4.sonjumoney.domain.Event;
 import com.hana4.sonjumoney.domain.Family;
 import com.hana4.sonjumoney.domain.enums.EventCategory;
@@ -10,9 +11,21 @@ import com.hana4.sonjumoney.domain.enums.EventCategory;
 import lombok.Builder;
 
 @Builder
-public record EventAddRequest(EventCategory eventCategory,
-							  String eventName, LocalDate startDate, LocalDate endDate,
-							  List<Long> eventParticipantsId) {
+public record EventAddRequest(
+	@JsonProperty("event_category")
+	EventCategory eventCategory,
+
+	@JsonProperty("event_name")
+	String eventName,
+
+	@JsonProperty("start_date")
+	LocalDate startDate,
+
+	@JsonProperty("end_date")
+	LocalDate endDate,
+
+	@JsonProperty("event_participants")
+	List<Long> memberId) {
 	public Event toEntity(Family family) {
 		return Event.builder()
 			.family(family)
--- src/main/java/com/hana4/sonjumoney/service/EventService.java
+++ src/main/java/com/hana4/sonjumoney/service/EventService.java
@@ -32,7 +32,7 @@ public class EventService {
 		Event event = eventAddRequest.toEntity(family);
 		eventRepository.save(event);
 
-		List<Member> members = memberRepository.findAllById(eventAddRequest.eventParticipantsId());
+		List<Member> members = memberRepository.findAllById(eventAddRequest.memberId());
 		List<EventParticipant> eventParticipants = members.stream()
 			.map(member -> EventParticipant.builder()
 				.event(event)
