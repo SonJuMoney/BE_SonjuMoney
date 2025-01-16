2c22282085d5d7b6d8666f45aacaa0368944e894
daneng4 <daneng4@naver.com>
2025-01-16 10:53:06 +0900

    [Fix] EventConrollerTest 로직 수정

--- src/test/java/com/hana4/sonjumoney/controller/EventControllerTest.java
+++ src/test/java/com/hana4/sonjumoney/controller/EventControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -16,14 +15,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hana4.sonjumoney.ControllerTest;
-import com.hana4.sonjumoney.domain.Family;
-import com.hana4.sonjumoney.domain.Member;
-import com.hana4.sonjumoney.domain.User;
 import com.hana4.sonjumoney.domain.enums.EventCategory;
-import com.hana4.sonjumoney.domain.enums.MemberRole;
 import com.hana4.sonjumoney.domain.enums.NotifyStatus;
 import com.hana4.sonjumoney.dto.request.EventAddRequest;
 import com.hana4.sonjumoney.repository.FamilyRepository;
@@ -49,43 +45,14 @@ public class EventControllerTest extends ControllerTest {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@BeforeEach
-	public void before() throws Exception {
-
-		Family family = Family.builder()
-			.familyName("준용이네")
-			.build();
-		familyRepository.save(family);
-
-		User user1 = userRepository.findById(1L).get();
-		User user2 = userRepository.findById(2L).get();
-
-		Member member1 = Member.builder()
-			.family(family)
-			.memberRole(MemberRole.FATHER)
-			.user(user1)
-			.build();
-		memberRepository.save(member1);
-		Member member2 = Member.builder()
-			.family(family)
-			.memberRole(MemberRole.MOTHER)
-			.user(user2)
-			.build();
-		memberRepository.save(member2);
-
-	}
-
 	@Test
+	@Transactional
 	@DisplayName("일정 등록 테스트")
 	void addEventTest() throws Exception {
-		List<Long> memberIds = memberRepository.findAll().stream()
-			.map(Member::getId)
-			.toList();
-
 		EventAddRequest eventAddRequest = EventAddRequest.builder()
 			.eventCategory(EventCategory.MEMORIAL)
 			.eventName("결혼")
-			.memberId(memberIds)
+			.memberId(List.of(1L, 2L))
 			.startDate(LocalDate.of(2025, 1, 6))
 			.endDate(LocalDate.of(2025, 1, 6))
 			.notifyStatus(NotifyStatus.REGISTERED)
