2cacc9414d327cd9e2ffa085860aeaabfd1523d2
daneng4 <daneng4@naver.com>
2025-01-16 16:35:06 +0900

    [Fix] controllertest 상속하도록 수정

--- src/test/java/com/hana4/sonjumoney/controller/AccountControllerTest.java
+++ src/test/java/com/hana4/sonjumoney/controller/AccountControllerTest.java
@@ -3,48 +3,20 @@ package com.hana4.sonjumoney.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.Map;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hana4.sonjumoney.ControllerTest;
 import com.hana4.sonjumoney.dto.request.AccountRequest;
-import com.hana4.sonjumoney.dto.request.SignInRequest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class AccountControllerTest {
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Autowired
-	private ObjectMapper objectMapper;
-
-	private static String accessToken;
-
-	@BeforeAll
-	void setUp() throws Exception {
-		SignInRequest signInRequest = new SignInRequest("test1", "1234");
-		MvcResult mvcResult = mockMvc.perform(post("/api/auth/sign-in")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(signInRequest)))
-			.andExpect(status().isOk()).andReturn();
-
-		String responseBody = mvcResult.getResponse().getContentAsString();
-		Map<String, String> responseMap = objectMapper.readValue(responseBody, Map.class);
-		accessToken = responseMap.get("access_token");
-	}
+public class AccountControllerTest extends ControllerTest {
 
 	@Test
 	@DisplayName("계좌 등록 테스트")
