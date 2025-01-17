690a70c977dbe5d03162b53caeaed006c5acacb2
daneng4 <daneng4@naver.com>
2025-01-17 11:32:41 +0900

    [Test] 적금 계좌 생성 테스트 구현

--- src/test/java/com/hana4/sonjumoney/controller/AccountControllerTest.java
+++ src/test/java/com/hana4/sonjumoney/controller/AccountControllerTest.java
@@ -10,9 +10,11 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hana4.sonjumoney.ControllerTest;
 import com.hana4.sonjumoney.dto.request.CreateAccountRequest;
+import com.hana4.sonjumoney.dto.request.CreateSavingAccountRequest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +22,7 @@ import com.hana4.sonjumoney.dto.request.CreateAccountRequest;
 public class AccountControllerTest extends ControllerTest {
 
 	@Test
+	@Transactional
 	@DisplayName("계좌 등록 테스트")
 	void makeAccountTest() throws Exception {
 		String api = "/api/accounts";
@@ -37,7 +40,7 @@ public class AccountControllerTest extends ControllerTest {
 
 	@Test
 	@DisplayName("계좌 조회 테스트")
-	void getAccountTest()throws Exception {
+	void getAccountTest() throws Exception {
 		String api = "/api/accounts";
 
 		mockMvc.perform(get(api)
@@ -47,5 +50,32 @@ public class AccountControllerTest extends ControllerTest {
 			.andExpect(jsonPath("$.account_name").value("하나자유입출금통장"))
 			.andExpect(jsonPath("$.bank").value("하나은행"))
 			.andDo(print());
+	}
+
+	@Test
+	@Transactional
+	@DisplayName("적금 계좌 생성 테스트")
+	void createSavingAccountTest() throws Exception {
+		String api = "/api/accounts/savings";
+
+		CreateSavingAccountRequest request = CreateSavingAccountRequest.builder()
+			.accountTypeId(2L)
+			.message("착하게 자라야 한다~")
+			.holderResidentNum("121212-1212121")
+			.accountPassword("1234")
+			.autoTransferable(true)
+			.userId(2L)
+			.withdrawalAccountId(1L)
+			.depositAccountId(1L)
+			.payDay(3)
+			.payAmount(50000L)
+			.build();
+
+		mockMvc.perform(post(api)
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + accessToken)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isOk());
+
 	}
 }
