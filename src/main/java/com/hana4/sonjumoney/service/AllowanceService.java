4f0762df7e76ad23749b5d761e27914c07147faf
LimHyeongseok <limhs8639@gmail.com>
2025-01-18 13:11:21 +0900

    [Feat] 용돈 송금시 알림 전송

--- src/main/java/com/hana4/sonjumoney/service/AllowanceService.java
+++ src/main/java/com/hana4/sonjumoney/service/AllowanceService.java
@@ -1,5 +1,7 @@
 package com.hana4.sonjumoney.service;
 
+import com.hana4.sonjumoney.domain.enums.AlarmType;
+import com.hana4.sonjumoney.dto.response.SendAllowanceResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,12 +10,13 @@ import com.hana4.sonjumoney.domain.Allowance;
 import com.hana4.sonjumoney.domain.Member;
 import com.hana4.sonjumoney.dto.AllowanceDto;
 import com.hana4.sonjumoney.dto.CreateAllowanceDto;
-import com.hana4.sonjumoney.dto.global.CreatedDto;
 import com.hana4.sonjumoney.dto.request.SendAllowanceRequest;
 import com.hana4.sonjumoney.exception.CommonException;
 import com.hana4.sonjumoney.exception.ErrorCode;
 import com.hana4.sonjumoney.repository.AllowanceRepository;
 import com.hana4.sonjumoney.repository.MemberRepository;
+import com.hana4.sonjumoney.websocket.dto.AlarmDto;
+import com.hana4.sonjumoney.websocket.handler.WebsocketHandler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,15 +29,21 @@ public class AllowanceService {
 	private final MemberRepository memberRepository;
 	private final AccountService accountService;
 	private final FeedService feedService;
+	private final WebsocketHandler websocketHandler;
 
 	@Transactional
-	public CreatedDto sendAllowance(MultipartFile image, Long userId, SendAllowanceRequest sendAllowanceRequest) {
+	public SendAllowanceResponse sendAllowance(MultipartFile image, Long userId, SendAllowanceRequest sendAllowanceRequest) {
 		Member receiver = memberRepository.findById(sendAllowanceRequest.receiverId())
 			.orElseThrow(() -> new CommonException(
 				ErrorCode.NOT_FOUND_MEMBER));
 		Member sender = memberRepository.findByUser_IdAndFamily(userId, receiver.getFamily())
 			.orElseThrow(() -> new CommonException(
 				ErrorCode.NOT_FOUND_MEMBER));
+
+		if (!sender.getFamily().equals(receiver.getFamily())) {
+			throw new CommonException(ErrorCode.DIFFERENT_FAMILY);
+		}
+
 		accountService.makeTransferByUserId(AllowanceDto.of(sender.getUser().getId(), receiver.getUser().getId(),
 			sendAllowanceRequest.amount()));
 
@@ -43,7 +52,7 @@ public class AllowanceService {
 		);
 		Long feedId = feedService.saveAllowanceFeed(
 			CreateAllowanceDto.of(allowance, image, sendAllowanceRequest.message()));
-
-		return CreatedDto.of("송금을 완료했습니다.");
+		websocketHandler.sendMemberAlarm(AlarmDto.of(receiver.getId(), sender.getId(), AlarmType.ALLOWANCE));
+		return SendAllowanceResponse.of("송금을 완료했습니다.");
 	}
 }
