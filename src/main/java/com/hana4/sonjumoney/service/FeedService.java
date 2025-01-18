642ced454219fd6cbad3731ebc0b2f357495e70a
ImHyungsuk <limhs8630@gmail.com>
2025-01-18 17:46:05 +0900

    [Feat] S3애 이미지 업로드 구현 완성

--- src/main/java/com/hana4/sonjumoney/service/FeedService.java
+++ src/main/java/com/hana4/sonjumoney/service/FeedService.java
@@ -1,5 +1,7 @@
 package com.hana4.sonjumoney.service;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +12,9 @@ import com.hana4.sonjumoney.domain.FeedContent;
 import com.hana4.sonjumoney.domain.Member;
 import com.hana4.sonjumoney.domain.enums.FeedType;
 import com.hana4.sonjumoney.dto.CreateAllowanceDto;
+import com.hana4.sonjumoney.dto.ImagePrefix;
+import com.hana4.sonjumoney.exception.CommonException;
+import com.hana4.sonjumoney.exception.ErrorCode;
 import com.hana4.sonjumoney.repository.FeedContentRepository;
 import com.hana4.sonjumoney.repository.FeedRepository;
 
@@ -35,7 +40,14 @@ public class FeedService {
 
 		if (isExist) {
 			MultipartFile image = createAllowanceDto.image();
-			String contentUrl = s3Service.upload(image);
+			String contentUrl;
+
+			try {
+				contentUrl = s3Service.uploadImageToS3(image, ImagePrefix.ALLOWANCE);
+			} catch (IOException e) {
+				throw new CommonException(ErrorCode.IMAGE_UPLOAD_FAILED);
+			}
+
 			feedContentRepository.save(
 				new FeedContent(savedFeed, contentUrl)
 			);
