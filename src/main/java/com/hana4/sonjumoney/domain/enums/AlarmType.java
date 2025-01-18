582ece5e0d1caa09ddec49a115ef749e75a64a87
LimHyeongseok <limhs8639@gmail.com>
2025-01-18 13:10:15 +0900

    [Fix] 알림 유형 수정

--- src/main/java/com/hana4/sonjumoney/domain/enums/AlarmType.java
+++ src/main/java/com/hana4/sonjumoney/domain/enums/AlarmType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum AlarmType {
 
 	ALLOWANCE("용돈","용돈을 보내셨어요"),
-	RESPONSE("보내신 용돈의 답장","보내신 용돈의 답장이 왔어요"),
+	THANKS("감사메시지","보내신 용돈의 답장이 왔어요"),
 	SAVINGS("적금입금","메시지를 작성해주세요"),
 	FEED("피드","새로운 피드가 올라왔어요"),
 	EVENT("일정 등록","새로운 일정이 등록되었어요"),
@@ -16,7 +16,7 @@ public enum AlarmType {
 	BIRTHDAY("생일","우리 가족의 생일이에요"),
 	DINING("외식","가족 외식이 있어요"),
 	MEMORIAL("기념일","기념일이에요"),
-	OTHERS("기타","가족일정이 있어요"),
+	OTHERS("일정","가족일정이 있어요"),
 	INVITE("초대","가족초대를 요청했어요"),
 	;
 
