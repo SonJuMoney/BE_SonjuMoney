8d569e02b948d98395adc172e9755537c900ba59
LimHyeongseok <limhs8639@gmail.com>
2025-01-19 18:55:31 +0900

    [Test] 피드 삭제 api 테스트

--- src/main/java/com/hana4/sonjumoney/dto/response/CreateFeedResponse.java
+++ src/main/java/com/hana4/sonjumoney/dto/response/CreateFeedResponse.java
@@ -1,10 +1,14 @@
 package com.hana4.sonjumoney.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 
 @Builder
 public record CreateFeedResponse(
 	Integer code,
+	@JsonProperty(value = "feed_id")
+	Long feedId,
 	String message
 ) {
 	public static CreateFeedResponse of(Integer code, String message) {
--- src/test/java/com/hana4/sonjumoney/controller/FeedControllerTest.java
+++ src/test/java/com/hana4/sonjumoney/controller/FeedControllerTest.java
@@ -8,25 +8,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.nio.charset.StandardCharsets;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hana4.sonjumoney.ControllerTest;
+import com.hana4.sonjumoney.domain.Feed;
 import com.hana4.sonjumoney.dto.request.CreateFeedRequest;
+import com.hana4.sonjumoney.dto.response.CreateFeedResponse;
+import com.hana4.sonjumoney.exception.CommonException;
+import com.hana4.sonjumoney.exception.ErrorCode;
+import com.hana4.sonjumoney.repository.FeedRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FeedControllerTest extends ControllerTest {
 
+	@Autowired
+	private FeedRepository feedRepository;
+
+	private Long feedId;
+
 	@Test
-	void addFeedTest()throws Exception {
+	@Order(1)
+	void addFeedTest() throws Exception {
 		// given
 		CreateFeedRequest request = new CreateFeedRequest(1L, "즐거운 여행~");
 		MockMultipartFile image1 = new MockMultipartFile(
@@ -49,7 +66,7 @@ class FeedControllerTest extends ControllerTest {
 		);
 
 		String api = "/api/feeds";
-		mockMvc.perform(multipart(api)
+		ResultActions resultActions = mockMvc.perform(multipart(api)
 				.file(image1)
 				.file(image2)
 				.file(data)
@@ -58,6 +75,20 @@ class FeedControllerTest extends ControllerTest {
 			.andDo(print())
 			.andExpect(status().isCreated())
 			.andExpect(jsonPath("$.message").value("피드 등록이 완료되었습니다."));
+		CreateFeedResponse createFeedResponse = objectMapper.readValue(
+			resultActions.andReturn().getResponse().getContentAsString(), CreateFeedResponse.class);
+		feedId = createFeedResponse.feedId();
+	}
 
+	@Test
+	@Order(2)
+	void deleteFeedTest() throws Exception {
+		String api = "/api/feeds" + feedId;
+		mockMvc.perform(delete(api)
+				.header("Authorization", "Bearer " + accessToken))
+			.andDo(print())
+			.andExpect(status().isOk());
+
+		assert feedRepository.findById(feedId).isEmpty();
 	}
 }
