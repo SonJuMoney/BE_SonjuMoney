291edd42e871641bd2de3a8cfa495bac27af8f42
hs917ouo <hs917ouo@naver.com>
2025-01-20 17:37:43 +0900

    [Fix] 주민등록번호 중복 방지 코드 추가

--- src/main/java/com/hana4/sonjumoney/repository/UserRepository.java
+++ src/main/java/com/hana4/sonjumoney/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.hana4.sonjumoney.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hana4.sonjumoney.domain.User;
@@ -11,4 +10,6 @@ import com.hana4.sonjumoney.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByAuthId(String authId);
+
+	Optional<User> findByResidentNum(String residentNum);
 }
