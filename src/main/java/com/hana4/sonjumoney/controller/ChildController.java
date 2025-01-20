330008baed14ebe7d6e5d0992ecf2aa4ac8d06ce
MIJIN <larchlarix0309@gmail.com>
2025-01-20 09:18:21 +0900

    [Feat] GetChildrenResponse dto 생성

--- src/main/java/com/hana4/sonjumoney/controller/ChildController.java
+++ src/main/java/com/hana4/sonjumoney/controller/ChildController.java
@@ -1,7 +1,12 @@
 package com.hana4.sonjumoney.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hana4.sonjumoney.util.AuthenticationUtil;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,5 +16,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/children")
 public class ChildController {
+
+	private final ChildService childService;
+
+	@GetMapping()
+	public ResponseEntity<GetChildrenResponse> getChildren(Authentication authentication) {
+		Long userId = AuthenticationUtil.getUserId(authentication);
+		return ResponseEntity.ok().body(childService.getChildren(userId));
+	}
 
 }
