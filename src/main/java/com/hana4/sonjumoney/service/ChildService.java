7c84f9f5568825c22cf0b4052e4d8fd412109e22
MIJIN <larchlarix0309@gmail.com>
2025-01-20 11:13:08 +0900

    [Feat] 예외처리 추가

--- src/main/java/com/hana4/sonjumoney/service/ChildService.java
+++ src/main/java/com/hana4/sonjumoney/service/ChildService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.hana4.sonjumoney.domain.Relationship;
 import com.hana4.sonjumoney.dto.response.GetChildrenResponse;
+import com.hana4.sonjumoney.exception.CommonException;
+import com.hana4.sonjumoney.exception.ErrorCode;
 import com.hana4.sonjumoney.repository.RelationshipRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,9 @@ public class ChildService {
 
 	public List<GetChildrenResponse> getChildren(Long userId) {
 		List<Relationship> children = relationshipRepository.findAllByParent_Id(userId);
+		if (children.isEmpty()) {
+			throw new CommonException(ErrorCode.NOT_FOUND_DATA);
+		}
 		return children.stream().map(relationship ->
 			GetChildrenResponse.of(
 				relationship.getChild().getId(),
