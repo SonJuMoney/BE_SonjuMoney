42966883877b006163f9415b7ed04118e394c96a
ImHyungsuk <limhs8630@gmail.com>
2025-01-21 15:03:14 +0900

    [Feat] 알림 생성 메소드 구현

--- src/main/java/com/hana4/sonjumoney/service/AlarmService.java
+++ src/main/java/com/hana4/sonjumoney/service/AlarmService.java
@@ -8,15 +8,27 @@ import org.springframework.stereotype.Service;
 
 import com.hana4.sonjumoney.domain.Alarm;
 import com.hana4.sonjumoney.domain.enums.AlarmStatus;
+import com.hana4.sonjumoney.domain.Event;
+import com.hana4.sonjumoney.domain.Family;
+import com.hana4.sonjumoney.domain.Member;
+import com.hana4.sonjumoney.domain.User;
+import com.hana4.sonjumoney.domain.enums.AlarmType;
 import com.hana4.sonjumoney.dto.AlarmContentDto;
 import com.hana4.sonjumoney.dto.AlarmResultDto;
+import com.hana4.sonjumoney.dto.CreateAlarmDto;
+import com.hana4.sonjumoney.dto.SendAlarmDto;
 import com.hana4.sonjumoney.dto.response.AlarmResponse;
 import com.hana4.sonjumoney.dto.response.AlarmStatusResponse;
 import com.hana4.sonjumoney.dto.response.UpdateAlarmResponse;
 import com.hana4.sonjumoney.exception.CommonException;
 import com.hana4.sonjumoney.exception.ErrorCode;
 import com.hana4.sonjumoney.repository.AlarmRepository;
+import com.hana4.sonjumoney.repository.EventParticipantRepository;
+import com.hana4.sonjumoney.repository.EventRepository;
+import com.hana4.sonjumoney.repository.FamilyRepository;
+import com.hana4.sonjumoney.repository.MemberRepository;
 import com.hana4.sonjumoney.repository.UserRepository;
+import com.hana4.sonjumoney.websocket.handler.AlarmHandler;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,7 +37,12 @@ import lombok.RequiredArgsConstructor;
 public class AlarmService {
 
 	private final AlarmRepository alarmRepository;
+	private final MemberRepository memberRepository;
+	private final FamilyRepository familyRepository;
+	private final EventRepository eventRepository;
+	private final EventParticipantRepository eventParticipantRepository;
 	private final UserRepository userRepository;
+	private final AlarmHandler alarmHandler;
 	private static final int PAGE_SIZE = 30;
 
 	public AlarmResponse getAlarms(Long userId, Integer page) {
@@ -95,5 +112,94 @@ public class AlarmService {
 		List<Alarm> alarms = alarmRepository.findByUserIdAndAlarmStatus(userId, alarmStatus);
 		boolean result = !alarms.isEmpty();
 		return AlarmStatusResponse.of(result);
+	}
+	public void createOneOffAlarm(CreateAlarmDto createAlarmDto) {
+		AlarmType alarmType = createAlarmDto.alarmType();
+		Member sender = memberRepository.findById(createAlarmDto.senderId())
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_MEMBER));
+
+		switch (alarmType) {
+			case ALLOWANCE, THANKS, INVITE: {
+				Member receiver = memberRepository.findById(createAlarmDto.alarmSessionId())
+					.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_MEMBER));
+				String message = sender.getMemberRole().getValue() + "님이 " + alarmType.getMessage();
+
+				Alarm alarm = alarmRepository.save(
+					new Alarm(receiver.getUser(), alarmType, createAlarmDto.linkId(), message));
+				alarmHandler.sendUserAlarm(SendAlarmDto.from(alarm));
+				break;
+			}
+			case FEED: {
+				Family family = familyRepository.findById(createAlarmDto.alarmSessionId())
+					.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+				String message = alarmType.getMessage();
+				List<Member> members = memberRepository.findByFamilyId(family.getId());
+
+				for (Member member : members) {
+					Alarm alarm = alarmRepository.save(
+						new Alarm(member.getUser(), alarmType, createAlarmDto.linkId(), message));
+					alarmHandler.sendUserAlarm(SendAlarmDto.from(alarm));
+				}
+				break;
+			}
+			default: {
+				throw new CommonException(ErrorCode.ALARM_SEND_FAILED);
+			}
+		}
+	}
+
+	// TODO: 이건 개별적으로 안하고 한번에 보내게 바꾸기
+	public void createEventAlarm(CreateAlarmDto createAlarmDto) {
+		AlarmType alarmType = createAlarmDto.alarmType();
+		Member sender = memberRepository.findById(createAlarmDto.senderId())
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_MEMBER));
+		Event event = eventRepository.findById(createAlarmDto.linkId())
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+		String alarmMessage = makeEventAlarmMessage(createAlarmDto.linkId());
+
+		switch (alarmType) {
+			case BIRTHDAY:{
+				Family family = familyRepository.findById(event.getId())
+					.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+				List<Member> members = memberRepository.findByFamilyId(family.getId());
+
+				for (Member member : members) {
+					alarmRepository.save(
+						new Alarm(member.getUser(), alarmType, createAlarmDto.linkId(), alarmMessage));
+				}
+				break;
+			}
+			case TRAVEL, DINING, MEMORIAL, OTHERS:{
+				List<Member> participants = eventParticipantRepository.findMembersByEventId(event.getId());
+				participants.remove(sender);
+				for (Member participant : participants) {
+					alarmRepository.save(
+						new Alarm(participant.getUser(), alarmType, createAlarmDto.linkId(), alarmMessage));
+				}
+			}
+			default:{
+				throw new CommonException(ErrorCode.WRONG_ALARM_TYPE);
+			}
+		}
+
+	}
+
+	public void createSavingAlarm(CreateAlarmDto createAlarmDto) {
+		AlarmType alarmType = createAlarmDto.alarmType();
+		if (!alarmType.equals(AlarmType.SAVINGS)) {
+			throw new CommonException(ErrorCode.WRONG_ALARM_TYPE);
+		}
+		User user = userRepository.findById(createAlarmDto.alarmSessionId())
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+		String alarmMessage = "내일은 " + alarmType.getMessage();
+		alarmRepository.save(
+			new Alarm(user, alarmType, createAlarmDto.linkId(), alarmMessage));
+	}
+
+
+	private String makeEventAlarmMessage(Long eventId) {
+		Event event = eventRepository.findById(eventId)
+			.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+		return event.getNotifyStatus().getMessage() + event.getEventName() + "가 있어요.";
 	}
 }
