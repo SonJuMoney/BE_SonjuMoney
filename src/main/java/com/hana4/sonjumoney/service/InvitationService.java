937a77c8dd63ba470dd80423d4b3317009eb79fa
MIJIN <larchlarix0309@gmail.com>
2025-01-21 09:37:16 +0900

    [Feat] 초대 수락 api 로직 구현

---
+++ src/main/java/com/hana4/sonjumoney/service/InvitationService.java
@@ -0,0 +1,35 @@
+package com.hana4.sonjumoney.service;
+
+import org.springframework.stereotype.Service;
+
+import com.hana4.sonjumoney.domain.Invitation;
+import com.hana4.sonjumoney.domain.Member;
+import com.hana4.sonjumoney.exception.CommonException;
+import com.hana4.sonjumoney.exception.ErrorCode;
+import com.hana4.sonjumoney.repository.InvitationRepository;
+import com.hana4.sonjumoney.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InvitationService {
+	private final InvitationRepository invitationRepository;
+
+	private final MemberRepository memberRepository;
+
+	public Long acceptInvitation(Long userId, Long invitationId) {
+		Invitation invitation = invitationRepository.findById(invitationId).orElseThrow(() -> new CommonException(
+			ErrorCode.NOT_FOUND_DATA));
+		if (!(invitation.getInvitee().getId().equals(userId))) {
+			throw new CommonException(ErrorCode.UNAUTHORIZED);
+		}
+		Member member = Member.builder()
+			.family(invitation.getFamily())
+			.user(invitation.getInvitee())
+			.memberRole(invitation.getMemberRole())
+			.build();
+		memberRepository.save(member);
+		return invitation.getFamily().getId();
+	}
+}
