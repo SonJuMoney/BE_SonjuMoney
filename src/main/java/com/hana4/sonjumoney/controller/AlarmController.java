dba00025319ef42fe3e17db325cf4b2f3dc63734
hs917ouo <hs917ouo@naver.com>
2025-01-21 11:04:11 +0900

    [Feat] 알람 상태 확인 api 구현

--- src/main/java/com/hana4/sonjumoney/controller/AlarmController.java
+++ src/main/java/com/hana4/sonjumoney/controller/AlarmController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hana4.sonjumoney.domain.enums.AlarmStatus;
 import com.hana4.sonjumoney.dto.response.AlarmResponse;
+import com.hana4.sonjumoney.dto.response.AlarmStatusResponse;
 import com.hana4.sonjumoney.dto.response.UpdateAlarmResponse;
 import com.hana4.sonjumoney.service.AlarmService;
 import com.hana4.sonjumoney.util.AuthenticationUtil;
@@ -33,6 +35,14 @@ public class AlarmController {
 	@PatchMapping("/{alarm_id}")
 	public ResponseEntity<UpdateAlarmResponse> updateAlarm(@PathVariable("alarm_id") Long alarm_id) {
 		UpdateAlarmResponse response = alarmService.updateAlarm(alarm_id);
+		return ResponseEntity.ok().body(response);
+	}
+
+	@GetMapping("/status/{alarm_status}")
+	public ResponseEntity<AlarmStatusResponse> getAlarmStatus(@PathVariable("alarm_status") AlarmStatus alarmStatus,
+		Authentication authentication) {
+		Long userId = AuthenticationUtil.getUserId(authentication);
+		AlarmStatusResponse response = alarmService.getAlarmStatus(userId, alarmStatus);
 		return ResponseEntity.ok().body(response);
 	}
 }
