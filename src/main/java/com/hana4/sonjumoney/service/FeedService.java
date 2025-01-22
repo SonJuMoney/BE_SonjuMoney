73046a02ce0096e9a92bfed806652058ab292c97
hs917ouo <hs917ouo@naver.com>
2025-01-22 17:08:56 +0900

    [Feat] 피드 댓글 삭제 api 구현

--- src/main/java/com/hana4/sonjumoney/service/FeedService.java
+++ src/main/java/com/hana4/sonjumoney/service/FeedService.java
@@ -25,6 +25,7 @@ import com.hana4.sonjumoney.dto.FeedResultDto;
 import com.hana4.sonjumoney.dto.request.CreateFeedRequest;
 import com.hana4.sonjumoney.dto.request.PostFeedCommentRequest;
 import com.hana4.sonjumoney.dto.response.CreateFeedResponse;
+import com.hana4.sonjumoney.dto.response.DeleteFeedCommentResponse;
 import com.hana4.sonjumoney.dto.response.DeleteFeedResponse;
 import com.hana4.sonjumoney.dto.response.FeedLikeResponse;
 import com.hana4.sonjumoney.dto.response.FeedResponse;
@@ -253,9 +254,6 @@ public class FeedService {
 		PostFeedCommentRequest postFeedCommentRequest) {
 		Feed feed = feedRepository.findById(feedId).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
 		Long familyId = feed.getMember().getFamily().getId();
-		if (!memberRepository.existsByUserIdAndFamilyId(userId, familyId)) {
-			throw new CommonException(ErrorCode.FORBIDDEN);
-		}
 		try {
 			Member member = memberRepository.findByUserIdAndFamilyId(userId, familyId)
 				.orElseThrow(() -> new CommonException(ErrorCode.FORBIDDEN));
@@ -271,6 +269,29 @@ public class FeedService {
 			throw new CommonException(ErrorCode.INTERNAL_SERVER_ERROR);
 		}
 		return PostFeedCommentResponse.builder()
+			.code(200)
+			.message("요청 성공")
+			.build();
+	}
+
+	public DeleteFeedCommentResponse deleteFeedComment(Long userId, Long feedId, Long commentId) {
+		Feed feed = feedRepository.findById(feedId).orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+		Long familyId = feed.getMember().getFamily().getId();
+		try {
+			Member member = memberRepository.findByUserIdAndFamilyId(userId, familyId)
+				.orElseThrow(() -> new CommonException(ErrorCode.FORBIDDEN));
+			Comment comment = commentRepository.findById(commentId)
+				.orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_DATA));
+			if (!comment.getMember().equals(member)) {
+				throw new CommonException(ErrorCode.FORBIDDEN);
+			}
+			commentRepository.delete(comment);
+		} catch (CommonException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new CommonException(ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+		return DeleteFeedCommentResponse.builder()
 			.code(200)
 			.message("요청 성공")
 			.build();
