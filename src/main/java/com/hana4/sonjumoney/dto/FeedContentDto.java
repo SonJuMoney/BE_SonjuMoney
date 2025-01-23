835a86b206e07d1efbc3112f49ecb99519476d07
hs917ouo <hs917ouo@naver.com>
2025-01-23 11:13:06 +0900

    [Fix] 피드에 역할 추가

--- src/main/java/com/hana4/sonjumoney/dto/FeedContentDto.java
+++ src/main/java/com/hana4/sonjumoney/dto/FeedContentDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.hana4.sonjumoney.domain.enums.FeedType;
+import com.hana4.sonjumoney.domain.enums.MemberRole;
 
 import lombok.Builder;
 
@@ -12,6 +13,7 @@ public record FeedContentDto(
 	Long feed_id,
 	Long writer_id,
 	String writer_name,
+	MemberRole member_role,
 	Boolean is_mine,
 	String writer_image,
 	FeedType feed_type,
@@ -22,7 +24,8 @@ public record FeedContentDto(
 	List<FeedContentContentDto> contents,
 	List<FeedContentCommentDto> comments
 ) {
-	public static FeedContentDto of(Long feed_id, Long writer_id, String writer_name, Boolean is_mine,
+	public static FeedContentDto of(Long feed_id, Long writer_id, String writer_name, MemberRole member_role,
+		Boolean is_mine,
 		String writer_image, FeedType feed_type, String message, Integer like, Boolean is_update,
 		LocalDateTime created_at,
 		List<FeedContentContentDto> contents, List<FeedContentCommentDto> comments) {
@@ -30,6 +33,7 @@ public record FeedContentDto(
 			.feed_id(feed_id)
 			.writer_id(writer_id)
 			.writer_name(writer_name)
+			.member_role(member_role)
 			.is_mine(is_mine)
 			.writer_image(writer_image)
 			.feed_type(feed_type)
