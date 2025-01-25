f15a1b493931c50e3168b2c7e1bbf44bf4ac6c69
hs917ouo <hs917ouo@naver.com>
2025-01-26 00:35:18 +0900

    [Feat] 프로필 이미지 변경 api 구현

--- src/main/java/com/hana4/sonjumoney/domain/User.java
+++ src/main/java/com/hana4/sonjumoney/domain/User.java
@@ -14,6 +14,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -49,6 +50,7 @@ public class User {
 	private Gender gender;
 
 	@Column(name = "profile_link", length = 500)
+	@Setter(AccessLevel.PUBLIC)
 	private String profileLink;
 
 	@Builder
