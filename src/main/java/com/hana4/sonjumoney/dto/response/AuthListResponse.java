bd3c5eb96932a0e2ab08c99b83ad9814744c19bf
hs917ouo <hs917ouo@naver.com>
2025-01-25 17:07:54 +0900

    [Fix] 계정 전환 전 정보 프로필 및 성별 추가

--- src/main/java/com/hana4/sonjumoney/dto/response/AuthListResponse.java
+++ src/main/java/com/hana4/sonjumoney/dto/response/AuthListResponse.java
@@ -1,6 +1,7 @@
 package com.hana4.sonjumoney.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hana4.sonjumoney.domain.enums.Gender;
 import com.hana4.sonjumoney.domain.enums.Role;
 
 import lombok.Builder;
@@ -10,13 +11,17 @@ public record AuthListResponse(
 	@JsonProperty(value = "user_id")
 	Long userId,
 	Role role,
-	String name
+	String name,
+	String profile,
+	Gender gender
 ) {
-	public static AuthListResponse of(Long userId, Role role, String name) {
+	public static AuthListResponse of(Long userId, Role role, String name, String profile, Gender gender) {
 		return AuthListResponse.builder()
 			.userId(userId)
 			.role(role)
 			.name(name)
+			.profile(profile)
+			.gender(gender)
 			.build();
 	}
 }
