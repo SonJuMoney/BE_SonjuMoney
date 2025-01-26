bb9b4ec7b9c3b011475009c9334c710b64bacf14
hs917ouo <hs917ouo@naver.com>
2025-01-26 23:34:58 +0900

    [Chore] cors url 수정

--- src/main/java/com/hana4/sonjumoney/security/config/SecurityConfig.java
+++ src/main/java/com/hana4/sonjumoney/security/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/api/auth/sign-up", "/api/auth/sign-in", "/api/auth/id-duplication",
-					"/api/auth/reissue","/ws/alarms")
+					"/api/auth/reissue", "/ws/alarms")
 				.permitAll()
 				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui/index.html").permitAll()
 				.anyRequest()
@@ -86,7 +86,8 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOrigin("http://localhost:3000");
+		// configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedOrigin("https://sonjumoney.topician.com");
 		configuration.addAllowedMethod("*");
 		configuration.addAllowedHeader("*");
 		configuration.setAllowCredentials(true);
