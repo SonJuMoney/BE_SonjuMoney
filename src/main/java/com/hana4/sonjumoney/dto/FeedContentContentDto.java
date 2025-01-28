a83392637a1f71d060286244f339acd63c5ce647
ImHyungsuk <limhs8630@gmail.com>
2025-01-28 19:21:21 +0900

    [Refactor] Dto 생성자 수정

--- src/main/java/com/hana4/sonjumoney/dto/FeedContentContentDto.java
+++ src/main/java/com/hana4/sonjumoney/dto/FeedContentContentDto.java
@@ -1,6 +1,7 @@
 package com.hana4.sonjumoney.dto;
 
 import com.hana4.sonjumoney.domain.enums.ContentType;
+import com.hana4.sonjumoney.util.ContentUtil;
 
 import lombok.Builder;
 
@@ -9,6 +10,15 @@ public record FeedContentContentDto(
 	String url,
 	ContentType content_type
 ) {
+	public static FeedContentContentDto from(String url) {
+		String extension = ContentUtil.getExtension(url);
+		ContentType contentType = ContentUtil.classifyContentType(extension);
+		return FeedContentContentDto.builder()
+			.url(url)
+			.content_type(contentType)
+			.build();
+	}
+
 	public static FeedContentContentDto of(String url, ContentType content_type) {
 		return FeedContentContentDto.builder()
 			.url(url)
