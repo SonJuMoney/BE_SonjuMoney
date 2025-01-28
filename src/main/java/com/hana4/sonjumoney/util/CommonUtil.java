8a41aeb13729fbfad255b5d98c36f0fbe9275629
hs917ouo <hs917ouo@naver.com>
2025-01-28 21:41:55 +0900

    [Feat] 추천주제 생성 api 구현

--- src/main/java/com/hana4/sonjumoney/util/CommonUtil.java
+++ src/main/java/com/hana4/sonjumoney/util/CommonUtil.java
@@ -1,8 +1,11 @@
 package com.hana4.sonjumoney.util;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.hana4.sonjumoney.domain.enums.Gender;
+import com.hana4.sonjumoney.domain.enums.MemberRole;
 import com.hana4.sonjumoney.exception.CommonException;
 import com.hana4.sonjumoney.exception.ErrorCode;
 
@@ -40,5 +43,63 @@ public class CommonUtil {
 			throw new CommonException(ErrorCode.BAD_REQUEST);
 		}
 		return LocalDateTime.of(year, month, day, 0, 0, 0, 0);
+	}
+
+	public static List<MemberRole> getMemberRoles(Integer userBirth, Gender userGender, Integer targetBirth,
+		Gender targetGender) {
+		List<MemberRole> memberRoles = new ArrayList<MemberRole>();
+		if (userBirth > targetBirth) { // user가 더 어림
+			if (userGender.equals(Gender.MALE) && targetGender.equals(Gender.MALE)) {
+				memberRoles.add(MemberRole.SON);
+				memberRoles.add(MemberRole.GRANDFATHER);
+			} else if (userGender.equals(Gender.MALE) && targetGender.equals(Gender.FEMALE)) {
+				memberRoles.add(MemberRole.SON);
+				memberRoles.add(MemberRole.GRANDMOTHER);
+			} else if (userGender.equals(Gender.FEMALE) && targetGender.equals(Gender.MALE)) {
+				memberRoles.add(MemberRole.DAUGHTER);
+				memberRoles.add(MemberRole.GRANDFATHER);
+			} else if (userGender.equals(Gender.FEMALE) && targetGender.equals(Gender.FEMALE)) {
+				memberRoles.add(MemberRole.DAUGHTER);
+				memberRoles.add(MemberRole.GRANDMOTHER);
+			}
+		} else {
+			if (userGender.equals(Gender.MALE) && targetGender.equals(Gender.MALE)) {
+				memberRoles.add(MemberRole.GRANDFATHER);
+				memberRoles.add(MemberRole.SON);
+			} else if (userGender.equals(Gender.MALE) && targetGender.equals(Gender.FEMALE)) {
+				memberRoles.add(MemberRole.GRANDMOTHER);
+				memberRoles.add(MemberRole.SON);
+			} else if (userGender.equals(Gender.FEMALE) && targetGender.equals(Gender.MALE)) {
+				memberRoles.add(MemberRole.GRANDFATHER);
+				memberRoles.add(MemberRole.DAUGHTER);
+			} else if (userGender.equals(Gender.FEMALE) && targetGender.equals(Gender.FEMALE)) {
+				memberRoles.add(MemberRole.GRANDMOTHER);
+				memberRoles.add(MemberRole.DAUGHTER);
+			}
+		}
+		return memberRoles;
+	}
+
+	public static String translateRole(MemberRole memberRole) {
+		String role;
+		switch (memberRole) {
+			case SON -> role = "손자";
+			case DAUGHTER -> role = "손녀";
+			default -> role = memberRole.getValue();
+		}
+		return role;
+	}
+
+	public static Integer traslateBirth(String residentNum) {
+		String year = residentNum.substring(0, 2);
+		char code = residentNum.charAt(7);
+		if (code == '1' || code == '2') {
+			year = "19" + year;
+		} else if (code == '3' || code == '4') {
+			year = "20" + year;
+		} else {
+			throw new CommonException(ErrorCode.BAD_REQUEST);
+		}
+		return Integer.parseInt(year);
 	}
 }
---
+++ src/test/java/com/hana4/sonjumoney/controller/CallControllerTest.java
@@ -0,0 +1,32 @@
+package com.hana4.sonjumoney.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import com.hana4.sonjumoney.ControllerTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CallControllerTest extends ControllerTest {
+
+	@Test
+	void getRecommendationsTest() throws Exception {
+		String api = "/api/calls/recommendations";
+		mockMvc.perform(get(api + "/2")
+				.header("Authorization", "Bearer " + accessToken)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].topic").isNotEmpty())
+			.andExpect(jsonPath("$[1].topic").isNotEmpty())
+			.andExpect(jsonPath("$[2].topic").isNotEmpty())
+			.andExpect(jsonPath("$[3].topic").isNotEmpty())
+			.andExpect(jsonPath("$[4].topic").isNotEmpty());
+	}
+}
