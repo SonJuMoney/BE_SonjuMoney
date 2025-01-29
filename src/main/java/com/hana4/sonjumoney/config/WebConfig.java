a5adf4e4c38f71a72520e09a61f37d4cd1b0a64f
hs917ouo <hs917ouo@naver.com>
2025-01-29 21:13:10 +0900

    [Chore] cors 전체 허용

--- src/main/java/com/hana4/sonjumoney/config/WebConfig.java
+++ src/main/java/com/hana4/sonjumoney/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
-			.allowedOrigins("https://sonjumoney.topician.com")
+			// .allowedOrigins("http://localhost:3000")
+			// .allowedOrigins("https://sonjumoney.topician.com")
+			.allowedOriginPatterns("*")
 			.allowedMethods("GET", "POST", "PATCH", "DELETE")
 			.allowedHeaders("*")
 			.allowCredentials(true);
