04eb0267ce898bb19c7cc6e87e9956b22e174a52
ImHyungsuk <limhs8630@gmail.com>
2025-02-03 16:35:43 +0900

    [Test] 내 아이 조회 test 코드 수정

--- src/test/java/com/hana4/sonjumoney/controller/UserControllerTest.java
+++ src/test/java/com/hana4/sonjumoney/controller/UserControllerTest.java
@@ -4,9 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.io.IOException;
+import java.util.Map;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,13 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hana4.sonjumoney.dto.JwtTokenDto;
 import com.hana4.sonjumoney.dto.request.SignInRequest;
-import com.hana4.sonjumoney.service.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -79,6 +78,21 @@ public class UserControllerTest {
 			.andExpect(jsonPath("$[0].user_id").value(3))
 			.andExpect(jsonPath("$[0].user_name").value("용돈 받을 애"))
 			.andDo(print());
+
+		SignInRequest signInRequest = new SignInRequest("test2", "1234");
+		MvcResult mvcResult = mockMvc.perform(post("/api/v1/auth/sign-in")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(signInRequest)))
+			.andExpect(status().isOk()).andReturn();
+
+		String responseBody = mvcResult.getResponse().getContentAsString();
+		Map<String, String> responseMap = objectMapper.readValue(responseBody, Map.class);
+		accessToken = responseMap.get("access_token");
+
+		mockMvc.perform(get("/api/v1/users/children")
+				.header("Authorization", "Bearer " + accessToken)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().is4xxClientError());
 	}
 
 	@Test
